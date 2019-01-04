6fb6fa4acb90d886af0f3f92bcc786daf3d56206
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2019-01-04 19:35:26 +0000

    removed parallax-scrolling (hard to maintain)

--- src/parallax-scrolling.js
+++ src/parallax-scrolling.js
@@ -1,32 +1,21 @@
 let bgv = document.getElementsByClassName('bg-video')[0]
-let container = document.getElementsByClassName('container')[0]
 let magicHat = document.getElementsByClassName('nav-hat')[0]
 
 export function initScrolling () {
   window.addEventListener('scroll', () => {
-    window.requestAnimationFrame(() => {
-      // parallax scrolling video
-      if (bgv.getBoundingClientRect().bottom + 100 < container.getBoundingClientRect().top) {
-        // bgv does not reache bottom
-        bgv.style.top = window.scrollY + 'px'
-      } else if (parseInt(bgv.style.top) > window.scrollY) {
-        bgv.style.top = window.scrollY + 'px'
-      }
+    // magic hat
+    let top = bgv.getBoundingClientRect().bottom
+    if (top <= 0) {
+      magicHat.style.display = 'block'
+    } else {
+      magicHat.style.display = 'none'
+    }
 
-      // magic hat
-      let top = bgv.getBoundingClientRect().bottom
-      if (top <= 0) {
-        magicHat.style.display = 'block'
-      } else {
-        magicHat.style.display = 'none'
-      }
-    })
+    // magic hat navigation
+    magicHat.onclick = () => {
+      window.scrollTo(0, 0)
+    }
   })
-  // magic hat navigation
-  magicHat.onclick = () => {
-    window.scrollTo(0, 0)
-  }
-  window.dispatchEvent(new window.Event('scroll'))
 }
 
 export function slowScrollTo (target) {
@@ -44,6 +33,6 @@ export function slowScrollTo (target) {
         distanceToScroll -= 10
         window.scrollBy(0, 10)
       }
-    }, 2)
+    }, 1)
   }, 0)
 }
