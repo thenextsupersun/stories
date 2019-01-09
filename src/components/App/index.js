4b3f04c7b0a408e99fadc47ddece15c28d380af9
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2019-01-09 22:26:09 +0000

    Using React to rewrite the blog.

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Home from '../Home'
+import './index.css'
+
+class App extends React.Component {
+  render () {
+    return <Home />
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-import { initScrolling } from './parallax-scrolling.js'
-import { initBlog } from './blog.js'
-import { initLife } from './life'
-import { initPlayer } from './player.js'
-import { initRouter } from './h5-router.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
 
-import './css/markdown.css'
-import './css/highlight.css'
-import './css/basic.css'
-import './css/navigator.css'
-import './css/blog.css'
-import './css/life.css'
-import './css/me.css'
+ReactDOM.render(<App />, document.getElementById('root'))
 
-window.addEventListener('load', () => {
-  initScrolling()
-  initBlog()
-  initLife()
-  initPlayer()
-  initRouter(['blog', 'life', 'me'])
-})
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
