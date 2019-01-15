3e3f38f52392b97a5c2437b54badf040998019c1
ZHIMIN ZHANG <77595103@zhiminzhang.com>
2019-01-15 19:20:45 +0000

    react router + react media to better responsive design

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Media from 'react-media'
 import { Route, Switch } from 'react-router-dom'
 
 import SideBar from './SideBar'
@@ -10,20 +11,35 @@ export default class Blog extends React.Component {
   state = {}
 
   componentDidMount () {
-    this.fetchBlogList().then(list =>
+    console.log('来一次')
+    this.fetchBlogList().then(list => {
       this.setState({ list: list.sort((a1, a2) => a2 - a1) })
-    )
+    })
   }
 
   render () {
     let list = this.state.list
+    let match = this.props.match
     let className = classes.Blog
     if (this.props.className) className += ' ' + this.props.className
     return (
-      <div className={className}>
-        <SideBar list={list} className={classes['Blog-sidebar']} />
-        {list ? this.renderArticleRouter() : null}
-      </div>
+      <Media query='(max-width: 500px)'>
+        {isPhone => {
+          return isPhone ? (
+            <div className={className}>
+              {match && match.isExact
+                ? <SideBar list={list} className={classes['Blog-sidebar']} />
+                : (list ? this.renderArticleRouter() : null)
+              }
+            </div>
+          ) : (
+            <div className={className}>
+              <SideBar list={list} className={classes['Blog-sidebar']} />
+              {list ? this.renderArticleRouter() : null}
+            </div>
+          )
+        }}
+      </Media>
     )
   }
 
