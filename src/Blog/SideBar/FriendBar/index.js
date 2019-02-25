055259dad7beae563a32f74ae04ffc1fd641c73d
ZHIMIN ZHNAG <77595103@zhiminzhang.com>
2019-02-25 12:41:28 +0800

    添加了友链

---
+++ src/Blog/SideBar/FriendBar/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import classes from './index.module.css'
+
+export default function Navigator ({ friends }) {
+  return (
+    <div className={classes['wrapper']}>
+      <h2 className={classes['title']}>一些表面兄弟...</h2>
+      {
+        friends.map(friend =>
+          <a href={friend.url} className={classes['column']}>
+            <img alt='avatar' className={classes['avatar']} src={friend.avatar} />
+            <div className={classes['info']}>
+              <h3 className={classes['name']}>{friend.name}</h3>
+              <span>{friend.description}</span>
+            </div>
+          </a>
+        )
+      }
+    </div>
+  )
+}
--- src/Blog/SideBar/index.js
+++ src/Blog/SideBar/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import InfoBar from './InfoBar'
 import ListBar from './ListBar'
+import FriendBar from './FriendBar'
 
 import classes from './index.module.css'
 
@@ -15,6 +16,26 @@ export default function SideBar (props) {
     <div className={className}>
       <InfoBar />
       <ListBar list={props.list} />
+      <FriendBar friends={[
+        {
+          name: '嘉兴侠客山姆赵',
+          description: '我讨厌语法糖, 搞不好会有牙疼的风险',
+          avatar: 'https://avatars2.githubusercontent.com/u/17445431?s=460&v=4',
+          url: 'https://github.com/wasamisam0119'
+        },
+        {
+          name: '嘉兴侠客山姆赵',
+          description: '我讨厌语法糖, 搞不好会有牙疼的风险',
+          avatar: 'https://avatars2.githubusercontent.com/u/17445431?s=460&v=4',
+          url: 'https://github.com/wasamisam0119'
+        },
+        {
+          name: '嘉兴侠客山姆赵',
+          description: '我讨厌语法糖, 搞不好会有牙疼的风险',
+          avatar: 'https://avatars2.githubusercontent.com/u/17445431?s=460&v=4',
+          url: 'https://github.com/wasamisam0119'
+        }
+      ]} />
     </div>
   )
 }
